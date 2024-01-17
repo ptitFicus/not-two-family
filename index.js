2ad2ef2ef84fb53efcdb20ed066d1dc60d01394d
Quentin Aubert <quentino.vega@gmail.com>
2024-01-17 10:36:49 +0100

    maybe ready

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const fastify = Fastify({
 })
 
 const port = process.env.PORT || 3000;
-
-const deeplKey = process.env.DEEPL_APIKEY || 'secret'
+// const deeplKey = process.env.DEEPL_APIKEY || 'secret'
+// const izanamiNewTranslationFeature = '010671d5-2bbb-45aa-a00c-a8a469638c9c';
 
 const dictionnary = loadDictionnary();
 const translations = loadTranslations();
@@ -138,41 +138,50 @@ function translate(texts) {
   return Promise.resolve(englishResult)
 }
 
-function betaTranslate(text, lang = "EN") {
-  return fetch("https://api-free.deepl.com/v2/translate", {
-    method: "POST",
-    headers: {
-      Authorization: `DeepL-Auth-Key ${deeplKey}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      text: [text],
-      'target_lang': lang,
-      'source_lang': "FR"
-    }),
-  })
-    .then(r => r.json())
-    .then(({ translations }) => {
-      const [{ text }] = translations
-      return text
-    })
-}
+// function betaTranslate(text, lang = "EN") {
+//   return fetch("https://api-free.deepl.com/v2/translate", {
+//     method: "POST",
+//     headers: {
+//       Authorization: `DeepL-Auth-Key ${deeplKey}`,
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       text: [text],
+//       'target_lang': lang,
+//       'source_lang': "FR"
+//     }),
+//   })
+//     .then(r => r.json())
+//     .then(({ translations }) => {
+//       const [{ text }] = translations
+//       return text
+//     })
+// }
 
 const translationHandler = (request, response) => {
-  const { text } = request.body
+  const { text, lang } = request.body
 
-  Promise.all([
-    fetch('http://izanami.oto.tools/api/v2/features?projects=3c2bf610-7e7a-49fe-9360-86403e3fc351&features=010671d5-2bbb-45aa-a00c-a8a469638c9c&user=fifou', {
-      headers: {
-        "izanami-client-id": 'dtc_D3WqdXbo9pyD5xaP',
-        "izanami-client-secret": 'dtc_nK8RIoHmUUSSxLMAwGVwwxaP95CPjslf02RKnnPXLEwJjoYF69R30YFJXoDrHaTp',
-      }
-    })
-      .then(r => r.json()),
+  // const headers = request.headers;
+  // const claim = headers["otoroshi-claim"];
+
+  // const decodedClaim = jwt.verify(claim, VERY_SECRET_PASSWORD, { issuer: 'Otoroshi' })
+  // const { izanamiUser } = decodedClaim.apikey.metadata;
+
+  return Promise.all([
+    // fetch(`http://izanami.oto.tools/api/v2/features?projects=3c2bf610-7e7a-49fe-9360-86403e3fc351&features=${izanamiNewTranslationFeature}&user=${izanamiUser}`, {
+    //   headers: {
+    //     "izanami-client-id": 'dtc_D3WqdXbo9pyD5xaP',
+    //     "izanami-client-secret": 'dtc_nK8RIoHmUUSSxLMAwGVwwxaP95CPjslf02RKnnPXLEwJjoYF69R30YFJXoDrHaTp',
+    //   }
+    // }).then(r => r.json()),
     fetchIPA(text)
   ])
-    .then(([{ active }, ipa]) => {
+    .then(([
+      // izanamiResponse, 
+      ipa]) => {
       console.log(`IPA : ${ipa}`);
+      // const active = izanamiResponse[izanamiNewTranslationFeature]?.active || false
+      const active = false;
       const decompositions = subArrayDecomposition(graphemeSplit(cleanIPA(ipa)));
 
       const results = [];
@@ -202,9 +211,9 @@ const translationHandler = (request, response) => {
     .then(([betaTranslation, results]) => {
       if (results.length === 0) {
         return []
-      } else if (betaTranslation) {
-        console.log('beta translation activated')
-        return Promise.all(results.map(r => betaTranslate(r, lang)))
+      // } else if (betaTranslation) {
+      //   console.log('beta translation activated')
+      //   return Promise.all(results.map(r => betaTranslate(r, lang)))
       } else {
         return translate(results)
       }
@@ -225,44 +234,44 @@ const translationHandler = (request, response) => {
 
 
 // ▂▃▅▇█▓▒░ Otoroshi exchange protocol ░▒▓█▇▅▃▂
-const VERY_SECRET_PASSWORD = 'secret';
+// const VERY_SECRET_PASSWORD = 'secret';
 
-function signToken(decodedState, _, res, next) {
-  const now = Math.floor(Date.now() / 1000)
+// function signToken(decodedState, _, res, next) {
+//   const now = Math.floor(Date.now() / 1000)
 
-  const token = {
-    'state-resp': decodedState.state,
-    iat: now,
-    nbf: now,
-    exp: now + 10,
-    aud: 'Otoroshi'
-  };
+//   const token = {
+//     'state-resp': decodedState.state,
+//     iat: now,
+//     nbf: now,
+//     exp: now + 10,
+//     aud: 'Otoroshi'
+//   };
 
-  res.header("Otoroshi-State-Resp", jwt.sign(token, VERY_SECRET_PASSWORD, { algorithm: 'HS512' }))
-  next();
-}
+//   res.header("Otoroshi-State-Resp", jwt.sign(token, VERY_SECRET_PASSWORD, { algorithm: 'HS512' }))
+//   next();
+// }
 
-function OtoroshiChallengeProtocol(req, res, done) {
-  const headers = req.headers;
-  const state = headers["otoroshi-state"];
+// function OtoroshiChallengeProtocol(req, res, done) {
+//   const headers = req.headers;
+//   const state = headers["otoroshi-state"];
 
-  jwt.verify(state, VERY_SECRET_PASSWORD, { issuer: 'Otoroshi' }, (err, decodedState) => {
-    if (err) {
-      res
-        .code(401)
-        .send({ error: "unauthorized" });
-    } else {
-      signToken(decodedState, res, res, done);
-    }
-  });
-}
+//   jwt.verify(state, VERY_SECRET_PASSWORD, { issuer: 'Otoroshi' }, (err, decodedState) => {
+//     if (err) {
+//       res
+//         .code(401)
+//         .send({ error: "unauthorized" });
+//     } else {
+//       signToken(decodedState, res, res, done);
+//     }
+//   });
+// }
 
 // ▂▃▅▇█▓▒░   The End    ░▒▓█▇▅▃▂
 
 
 
 
-fastify.addHook('onRequest', OtoroshiChallengeProtocol);
+// fastify.addHook('onRequest', OtoroshiChallengeProtocol);
 
 
 fastify.post("/translate", translationHandler)
